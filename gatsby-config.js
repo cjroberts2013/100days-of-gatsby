551abf3c79eba518a19427fdf29791a368e7ca01
CJ Roberts <cjroberts2013@gmail.com>
2021-02-12 20:23:45 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: "100DaysOfGatsby",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "fb17W8uwwOUXZd1O4K-ZJ5Q0BhWjD9NoStN-VlqwNPM",
+        spaceId: "22440r8nc9lr",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
